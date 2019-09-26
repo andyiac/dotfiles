9d32f8d517622e3163f5712177cf47edea439ade
andyiac <andyiaczhang@gmail.com>
2019-09-26 15:42:09 +0800

    update from android macbook

--- emacs/lisp/init.el
+++ emacs/lisp/init.el
@@ -29,7 +29,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (spaceline js2-mode spacemacs-theme nyan-mode helm-descbinds helm-projectile htmlize org-journal))))
+    (use-package-el-get spaceline js2-mode spacemacs-theme nyan-mode helm-descbinds helm-projectile htmlize org-journal))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
